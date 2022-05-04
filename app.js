5645eab2a42290bc40010f9536425f41a47141ce
jenna-graham <jclayto2@gmail.com>
2022-05-04 12:48:50 -0700

    created display mushrooms function

--- app.js
+++ app.js
@@ -78,9 +78,12 @@ function displayFriends() {
 
 function displayMushrooms() {
     // clear out the mushroom div
+    mushroomsEl.textContent = '';
 
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroomEl = renderMushroom(i);
+        mushroomsEl.append(mushroomEl);
     }
 }
 
